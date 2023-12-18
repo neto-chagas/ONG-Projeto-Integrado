395855326365695bff789e6d7f159127cf0e1bea
Neto Chagas <netochagas_mister@outlook.com>
2023-12-17 21:16:59 -0300

    seila que porra é essa bababouey

--- scripts/entrar.js
+++ scripts/entrar.js
@@ -20,21 +20,47 @@ function enviar(event) {
     return false;
   }
 
-  if (senhaInput.value.length < 8)
-  {
-
-    alert("A senha não é valida.");
+  if (senhaInput.value.length < 8) {
+    alert("A senha não é válida.");
     return false;
-
   }
 
+  registrarUsuario(senha, email);
+
   // Se todas as validações passarem, retorna true
+  alert("E-mail e senha válidos!");
   return true;
 }
 
 // Função para validar um endereço de e-mail
 function validarEmail(email) {
-  var re =
+  let re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
+}
+
+// Função para registrar um novo usuário
+function registrarUsuario(senha, email) {
+  // Cria um objeto de usuário
+  let usuario = {
+    senha: senha,
+    email: email,
+  };
+
+  // Converte o objeto de usuário em uma string JSON
+  let usuarioJSON = JSON.stringify(usuario);
+
+  // Armazena o usuário no LocalStorage
+  localStorage.setItem(email, usuarioJSON);
+}
+
+// Função para obter um usuário
+function getUsuario(email) {
+  // Obtém o usuário do LocalStorage
+  let usuarioJSON = localStorage.getItem(email);
+
+  // Converte a string JSON de volta em um objeto
+  let usuario = JSON.parse(usuarioJSON);
+
+  return usuario;
 }
